d4745a33d07c6962dd7846a25e0e016147b4c50b
Ayushi <ayushigupta9723@gmail.com>
2025-08-13 00:51:37 +0530

    add pipleines tfidf

---
+++ src/features/data_engineering.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+
+import os
+import yaml
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# fetch the data from data/processed
+train_data = pd.read_csv('./data/processed/train_processed.csv')
+test_data = pd.read_csv('./data/processed/test_processed.csv')
+
+train_data.fillna('',inplace=True)
+test_data.fillna('',inplace=True)
+
+# apply BoW
+X_train = train_data['content'].values
+y_train = train_data['sentiment'].values
+
+X_test = test_data['content'].values
+y_test = test_data['sentiment'].values
+
+max_features=yaml.safe_load(open('params.yaml','r'))['data_engineering']['max_features']
+
+# Apply Bag of Words (tfidfVectorizer)
+vectorizer = TfidfVectorizer(max_features=max_features)
+
+# Fit the vectorizer on the training data and transform it
+X_train_tfidf = vectorizer.fit_transform(X_train)
+
+# Transform the test data using the same vectorizer
+X_test_tfidf = vectorizer.transform(X_test)
+
+train_df = pd.DataFrame(X_train_tfidf.toarray())
+
+train_df['label'] = y_train
+
+test_df = pd.DataFrame(X_test_tfidf.toarray())
+
+test_df['label'] = y_test
+
+# store the data inside data/features
+data_path = os.path.join("data","features")
+
+os.makedirs(data_path)
+
+train_df.to_csv(os.path.join(data_path,"train_tfidf.csv"))
+test_df.to_csv(os.path.join(data_path,"test_tfidf.csv"))
